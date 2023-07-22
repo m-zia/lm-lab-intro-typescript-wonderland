6cfe8615e2f2e1ca9007a037ebfe09206e7fa181
M Zia <murtaza_zia@hotmail.co.uk>
2023-07-22 20:21:54 +0100

    completed chapter 5

--- src/chapter_5/chapter_5_queen_of_hearts.ts
+++ src/chapter_5/chapter_5_queen_of_hearts.ts
@@ -16,7 +16,7 @@ export function meetTheQueen(): void {
 
 	let guilty: boolean = false;
 
-	let witnesses: Witness[] = []; // 👉 FIXME ❌ - call getWitnesses here
+	let witnesses = getWitnesses();
 
 	if (!witnesses || witnesses.length === 0) {
 		print(`No witnesses have come forward to defend you.`);
@@ -45,7 +45,24 @@ export function meetTheQueen(): void {
 	}
 }
 
-// 👉 FIXME ❌ - this function needs writing to meet the above criteria
-function getWitnesses(): any {
-	return [];
+
+function getWitnesses(): Array<Witness> {
+	return [
+		{
+			name: 'The Mad Hatter',
+			giveEvidence: () => 'Not Guilty', 
+		},
+		{
+			name: 'The March Hare',
+			giveEvidence: () => { return 'Not Guilty' },
+		},
+		{
+			name: 'The Cheshire Cat',
+			giveEvidence: () => 'Not Guilty',
+		},
+		{
+			name: 'The White Rabbit',
+			giveEvidence: () => 'Not Guilty',
+		},
+	];
 }
